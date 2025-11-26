124169d121546661beaf52a1383af0ffe60ee2a1
James Freeman <jamesfreeman114@gmail.com>
2025-11-25 20:06:12 -0600

    Added save button to edit profile view

--- src/components/users/EditProfile.jsx
+++ src/components/users/EditProfile.jsx
@@ -1,12 +1,12 @@
 import "./Profile.css"
 import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useParams } from "react-router-dom"
-import { getUserById } from "../../services/userService"
+import { getUserById, updateUser } from "../../services/userService"
 
 export const EditProfile = () => {
 
-    
+    const navigate = useNavigate()
 
     const [user, setUser] = useState([])
     const {id} = useParams()
@@ -15,6 +15,28 @@ export const EditProfile = () => {
         getUserById(id).then((userObj) => { 
             setUser(userObj[0])})
     },[id])
+
+    const handleSave = (event) => {
+        event.preventDefault()
+
+        if (user.name && user.email && user.cohort) {
+            const editedUser = {
+                ...user,
+                name: user.name,
+                email: user.email,
+                cohort: user.cohort
+            }
+
+            updateUser(editedUser).then(()=>{
+                window.alert("Employee updated!")
+                navigate(`/profile/${user.id}`)
+            })
+        }
+
+        else {
+            window.alert("Please complete the form.")
+        }
+    }
 
 
 
@@ -68,7 +90,8 @@ export const EditProfile = () => {
             </fieldset>
             <fieldset>
                 <div className="form-group">
-                    <button className="form-btn btn-primary">Save</button>
+                    <button className="form-btn btn-primary"
+                            onClick={handleSave}>Save</button>
                 </div>
             </fieldset>
         </form>
